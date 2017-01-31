b0c0cc77430ef2055b289ec887ad43c805f3881e
Kate Sarah Böhner <Kate Sarah Bhner>
2017-01-31 16:55:37 +0000

    Initial commit

---
+++ Source/SploxRacers/SploxRacersGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "SploxRacersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLOXRACERS_API ASploxRacersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

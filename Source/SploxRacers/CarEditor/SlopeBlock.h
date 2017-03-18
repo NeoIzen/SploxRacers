ff818ceb3a06276b297874515f60a91c37e8c758
Kate Sarah Boehner <neoizen@gmail.com>
2017-03-18 19:53:05 +0000

    Added block library ui

---
+++ Source/SploxRacers/CarEditor/SlopeBlock.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CarEditor/BasicBlock.h"
+#include "SlopeBlock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLOXRACERS_API ASlopeBlock : public ABasicBlock
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
